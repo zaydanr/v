6611e5aa348bee834586349d26ff28b055b33cf9
zaydanr <134563317+zaydanr@users.noreply.github.com>
2024-04-08 13:14:21 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+
+firstM = 7
+
+
+def setup():
+  GPIO.setmode(GPIO.BCM)  # Numbers GPIOs by physical location
+  GPIO.setup(firstM, GPIO.OUT)
+  GPIO.output(firstM, GPIO.HIGH)
+
+
+def loop():
+  while True:
+    print('...relayd on')
+    GPIO.output(firstM, GPIO.LOW)
+    time.sleep(0.5)
+    print('relay off...')
+    GPIO.output(firstM, GPIO.HIGH)
+    time.sleep(0.5)
+
+
+def destroy():
+  GPIO.output(firstM, GPIO.HIGH)
+  GPIO.cleanup()  # Release resource
+
+
+if __name__ == '__main__':  # Program start from here
+  setup()
+  try:
+    loop()
+  except KeyboardInterrupt:
+    destroy()

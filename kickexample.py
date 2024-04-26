beba76ae752209dac523a6a2ce587ba9ac7f21b8
zaydanr <134563317+zaydanr@users.noreply.github.com>
2024-04-26 09:02:36 -0400

    Update kickexample.py

--- kickexample.py
+++ kickexample.py
@@ -9,31 +9,53 @@ kick_array = [0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 
 snare_array = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 hat_array = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-lengthofkick = len(kick_array)
+def setup():
+  GPIO.setmode(GPIO.BCM)  # Numbers GPIOs by physical location
+  GPIO.setup(kickMotor, GPIO.OUT)
+  GPIO.setup(snareMotor, GPIO.OUT)
+  GPIO.setup(hatMotor, GPIO.OUT)
+  GPIO.output(kickMotor, GPIO.LOW)
+  GPIO.output(snareMotor, GPIO.LOW)
+  GPIO.output(hatMotor, GPIO.LOW)
 
-while (1==1):
-    for x in kick_array:
-        if (x==1):
-            GPIO.output(kickMotor, GPIO.HIGH)
-            time.sleep(.5)
-        else:
-            GPIO.output(kickMotor, GPIO.LOW)
-            time.sleep(.5)
+def loop():
+    while (1==1):
+        for x in kick_array:
+            if (x==1):
+                GPIO.output(kickMotor, GPIO.HIGH)
+                time.sleep(.5)
+            else:
+                GPIO.output(kickMotor, GPIO.LOW)
+                time.sleep(.5)
 
-while (1==1):
-    for x in snare_array:
-        if (x==1):
-            GPIO.output(snareMotor, GPIO.HIGH)
-            time.sleep(.5)
-        else:
-            GPIO.output(snareMotor, GPIO.LOW)
-            time.sleep(.5)
+    while (1==1):
+        for x in snare_array:
+            if (x==1):
+                GPIO.output(snareMotor, GPIO.HIGH)
+                time.sleep(.5)
+            else:
+                GPIO.output(snareMotor, GPIO.LOW)
+                time.sleep(.5)
 
-while (1==1):
-    for x in hat_array:
-        if (x==1):
-            GPIO.output(hatMotor, GPIO.HIGH)
-            time.sleep(.5)
-        else:
-            GPIO.output(hatMotor, GPIO.LOW)
-            time.sleep(.5)
+    while (1==1):
+        for x in hat_array:
+            if (x==1):
+                GPIO.output(hatMotor, GPIO.HIGH)
+                time.sleep(.5)
+            else:
+                GPIO.output(hatMotor, GPIO.LOW)
+                time.sleep(.5)
+
+def destroy():
+  GPIO.output(kickMotor, GPIO.LOW)
+  GPIO.output(snareMotor, GPIO.LOW)
+  GPIO.output(hatMotor, GPIO.LOW)
+  GPIO.cleanup()  # Release resource
+
+
+if __name__ == '__main__':  # Program start from here
+  setup()
+  try:
+    loop()
+  except KeyboardInterrupt:
+    destroy()
